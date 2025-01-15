74289e4de3ada615f946092ca8b8dea8cc576017
mazida16 <mazidaa08@gmail.com>
2025-01-15 10:57:16 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,22 @@
+let nama = "Nurmazida Kayla Husnah";
+let alamat = "Perum Banjardowo Baru jl.Wijaya Kusuma";
+let usia = "16";
+let ttl = "Semarang 16 Mei 2008";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "mazida08@gmail.com";
+let berat = " 40";
+let tinggi = "146";
+let hobi = "menyanyi";
+let cita = "bos muda";
+let pekerjaan = "pelajar";
+let sekolah = "SMK N 1 Sayung";
+let jurusan = "PPLG";
+
+document.write("nama saya adalah " + nama + " " + "saya tinggal di "+ alamat + " "+ "pada tahun 2024 ini usia saya adalah "+ usia + " "+ "tahun."+ " "+
+"saya lahir di " + ttl + " " + "jenis kelamin saya adalah " + gender + " " + "saya beragama " + agama + " " + 
+"Alamat email saya adalah " + email + " " + "saya memiliki berat badan " + berat + " " + "kg " + " " + "dengan tinggi "
++ tinggi + " " + "cm "+ " " + "saya sangat senang " + hobi + " " + "karena saya memiliki cita-cita " + cita + " " +
+"saat ini saya adalah seorang " + pekerjaan + " " + "yang bersekolah di " + sekolah + " " + "pada jurusan " + 
+jurusan + " "
+ )
